ae670f0c95b3d4f748161eec6ae2eb428f7dc95b
AmrKhaled <kuro92717@gmail.com>
2026-04-28 02:00:09 +0300

    added changes in various folders

--- src/app.js
+++ src/app.js
@@ -121,7 +121,8 @@ app.use((err, req, res, next) => {
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("✅ MongoDB connected");
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);});
 });
 
 module.exports = app;
